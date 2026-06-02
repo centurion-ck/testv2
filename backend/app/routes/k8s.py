2399ec67626c86e865ccb865c8166b5ce7406161
Centurion K <centurion.k@kryptosinfosys.com>
2026-06-02 16:38:31 +0530

    sec3

--- backend/app/routes/k8s.py
+++ backend/app/routes/k8s.py
@@ -5,24 +5,12 @@ router = APIRouter()
 
 @router.get("/pods")
 def get_pods():
-
     try:
-
         result = subprocess.check_output(
-            [
-                "kubectl",
-                "get",
-                "pods",
-                "-A"
-            ]
+            ["kubectl", "get", "pods", "-A"]
         ).decode()
 
-        return {
-            "pods": result
-        }
+        return {"pods": result}
 
     except Exception as e:
-
-        return {
-            "error": str(e)
-        }
+        return {"error": str(e)}
